09e6072652f3e273075557eb57b5f9a793a92500
grambbledook <vodopyanovd@gmail.com>
2024-04-11 17:46:34 +0200

    simple app to listen for heart rate

--- hud/test.py
+++ hud/test.py
@@ -1,13 +1,37 @@
 import asyncio
-from bleak import BleakScanner
+from bleak import BleakScanner, BleakClient
 
-import bluetooth
-
+HEART_RATE_SERVICE_UUID = "0000180d-0000-1000-8000-00805f9b34fb"
+HEART_RATE_MEASUREMENT_CHAR_UUID = "00002a37-0000-1000-8000-00805f9b34fb"
 
 async def main():
-    print('Discovering bluetooth devices ...')
-    devices = await BleakScanner.discover(return_adv=True)
-    for i, d in enumerate(devices):
-        print(f"{i}: {d}")
+    scanner = BleakScanner()
+    devices = await scanner.discover()
 
-asyncio.run(main())
+    heart_rate_monitor = None
+    for device in devices:
+        if HEART_RATE_SERVICE_UUID in device.metadata["uuids"]:
+            heart_rate_monitor = device
+            break
+
+    if heart_rate_monitor is None:
+        print("Heart rate monitor not found.")
+        return
+
+    # Connect to the heart rate monitor
+    client = BleakClient(heart_rate_monitor)
+    await client.connect()
+
+    def on_data(sender: int, data: bytearray):
+        print(f"Heart rate: {data[1]}")
+
+    await client.start_notify(HEART_RATE_MEASUREMENT_CHAR_UUID, on_data)
+
+    await asyncio.sleep(10)
+
+    await client.stop_notify(HEART_RATE_MEASUREMENT_CHAR_UUID)
+
+    await client.disconnect()
+
+if __name__ == "__main__":
+    asyncio.run(main())
