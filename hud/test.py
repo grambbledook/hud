6cdb65e08a46c1115b2388884455e692f59e2039
grambbledook <vodopyanovd@gmail.com>
2024-04-11 17:56:28 +0200

    switch to advertisement data

--- hud/test.py
+++ hud/test.py
@@ -6,11 +6,11 @@ HEART_RATE_MEASUREMENT_CHAR_UUID = "00002a37-0000-1000-8000-00805f9b34fb"
 
 async def main():
     scanner = BleakScanner()
-    devices = await scanner.discover()
+    address_to_device_and_advertisement_data = await scanner.discover(return_adv=True)
 
     heart_rate_monitor = None
-    for device in devices:
-        if HEART_RATE_SERVICE_UUID in device.metadata["uuids"]:
+    for address, (device, advertisement_data) in address_to_device_and_advertisement_data.items():
+        if HEART_RATE_SERVICE_UUID in advertisement_data.service_uuids:
             heart_rate_monitor = device
             break
 
@@ -28,7 +28,6 @@ async def main():
     await client.start_notify(HEART_RATE_MEASUREMENT_CHAR_UUID, on_data)
 
     await asyncio.sleep(10)
-
     await client.stop_notify(HEART_RATE_MEASUREMENT_CHAR_UUID)
 
     await client.disconnect()
