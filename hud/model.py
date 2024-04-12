6e92a80d08e2d7ada70aaf802f930a014a4c679b
Denis V <vodopyanovd@gmail.com>
2024-04-12 11:16:39 +0200

    app model added

---
+++ hud/model.py
@@ -0,0 +1,40 @@
+from typing import Optional
+
+from hud.test import DeviceHandle
+
+
+class State(object):
+    def __init__(self):
+        self.heart_rate: Optional[int] = None
+        self.power: Optional[int] = None
+        self.cadence: Optional[int] = None
+        self.speed: Optional[float] = None
+
+
+class Devices(object):
+    def __init__(self):
+        self.power_meter: Optional[DeviceHandle[int]] = None
+        self.heart_rate_monitor: Optional[DeviceHandle[int]] = None
+        self.cadence_sensor: Optional[DeviceHandle[int]] = None
+        self.speed_sensor: Optional[DeviceHandle[float]] = None
+
+        self.state = State()
+
+    def set_heart_rate_monitor(self, device: DeviceHandle[int]):
+        if self.heart_rate_monitor:
+            self.heart_rate_monitor = device.unsubscribe()
+
+        self.heart_rate_monitor = device
+        self.heart_rate_monitor.subscribe(self._update_heart_rate)
+
+    def _update_heart_rate(self, heart_rate: int):
+        self.state.heart_rate = heart_rate
+
+    def _update_speed(self, speed: float):
+        self.state.speed = speed
+
+    def _update_power(self, power: int):
+        self.state.power = power
+
+    def _update_cadence(self, cadence: float):
+        self.state.cadence = cadence
--- hud/test.py
+++ hud/test.py
@@ -1,36 +1,64 @@
 import asyncio
-from bleak import BleakScanner, BleakClient
+from typing import AsyncGenerator, TypeVar, Generic, Callable
+
+from bleak import BleakScanner, BleakClient, BleakGATTCharacteristic
 
 HEART_RATE_SERVICE_UUID = "0000180d-0000-1000-8000-00805f9b34fb"
 HEART_RATE_MEASUREMENT_CHAR_UUID = "00002a37-0000-1000-8000-00805f9b34fb"
 
+T = TypeVar('T')
+
+
+class DeviceHandle(Generic[T]):
+    def __init__(self, name: str, client: BleakClient, service_uuid: str, characteristic_uuid: str):
+        self.name: str = name
+        self.service_uuid: str = service_uuid
+        self.characteristic_uuid: str = characteristic_uuid
+
+        self.client: BleakClient = client
+
+    async def subscribe(self, handle: Callable[[T], None]):
+        def on_data(characteristic: BleakGATTCharacteristic, data: bytearray) -> None:
+            print(f"Data from {characteristic}: {data}")
+            handle(data[1])
+
+        await self.client.connect()
+        await self.client.start_notify(self.characteristic_uuid, on_data)
+
+    async def unsubscribe(self):
+        if not self.client:
+            return
+
+        await self.client.stop_notify(self.characteristic_uuid)
+        await self.client.disconnect()
+
+
+class DeviceScanner(object):
+    def __init__(self, service_uuid: str, characteristic_uuid: str):
+        self.service_uuid = service_uuid
+        self.characteristic_uuid = characteristic_uuid
+
+    async def scan(self) -> AsyncGenerator[DeviceHandle, None]:
+        scanner = BleakScanner()
+        address_to_device_and_advertisement_data = await scanner.discover(return_adv=True)
+
+        for address, (device, advertisement_data) in address_to_device_and_advertisement_data.items():
+            if self.service_uuid in advertisement_data.service_uuids:
+                yield DeviceHandle(
+                    name=device.name or address,
+                    service_uuid=self.service_uuid,
+                    characteristic_uuid=self.characteristic_uuid,
+                    client=BleakClient(device),
+                )
+
+
 async def main():
-    scanner = BleakScanner()
-    address_to_device_and_advertisement_data = await scanner.discover(return_adv=True)
+    scanner = DeviceScanner(HEART_RATE_SERVICE_UUID, HEART_RATE_MEASUREMENT_CHAR_UUID)
+    async for device in scanner.scan():
+        await device.subscribe(lambda heart_rate: print(f"Heart rate: {heart_rate}"))
+        await asyncio.sleep(10)
+        await device.unsubscribe()
 
-    heart_rate_monitor = None
-    for address, (device, advertisement_data) in address_to_device_and_advertisement_data.items():
-        if HEART_RATE_SERVICE_UUID in advertisement_data.service_uuids:
-            heart_rate_monitor = device
-            break
-
-    if heart_rate_monitor is None:
-        print("Heart rate monitor not found.")
-        return
-
-    # Connect to the heart rate monitor
-    client = BleakClient(heart_rate_monitor)
-    await client.connect()
-
-    def on_data(sender: int, data: bytearray):
-        print(f"Heart rate: {data[1]}")
-
-    await client.start_notify(HEART_RATE_MEASUREMENT_CHAR_UUID, on_data)
-
-    await asyncio.sleep(10)
-    await client.stop_notify(HEART_RATE_MEASUREMENT_CHAR_UUID)
-
-    await client.disconnect()
 
 if __name__ == "__main__":
     asyncio.run(main())
